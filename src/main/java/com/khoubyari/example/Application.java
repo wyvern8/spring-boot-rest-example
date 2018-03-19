c8f77e071d7574733e65bfecb8f382537f105f00
AV <wyvern8@users.noreply.github.com>
2018-03-20 08:04:44 +1100

    Update Application.java

--- src/main/java/com/khoubyari/example/Application.java
+++ src/main/java/com/khoubyari/example/Application.java
@@ -25,7 +25,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class Application extends SpringBootServletInitializer {
 	
 	public boolean BREAKME = true; //tsnefkd
-	public boolean BREAKME2 = true; //tsnefkd
+	public boolean BREAKME2 = true; //tsnemfkd
 
     private static final Class<Application> applicationClass = Application.class;
     private static final Logger log = LoggerFactory.getLogger(applicationClass);
